1e2170ff3f223a47aa64a4c3cc3b3640b349905d
jakubhrdy <hrdy.jakub@gmail.com>
2014-05-28 17:15:00 +0200

    Blablabla

---
+++ NavalBattle/Ship.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavalBattle
+{
+    class Ship
+    {
+        private string[] pozice;
+        private string type;
+    }
+}
