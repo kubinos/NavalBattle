9f4518bbd1f2d56d8bd4f4fad022e59b553791a2
Marek Janský <paciik@paciik.eu>
2014-06-11 15:27:26 +0200

    MouseEvent

--- NavalBattle/GameWindow.xaml.cs
+++ NavalBattle/GameWindow.xaml.cs
@@ -22,12 +22,21 @@ namespace NavalBattle
         private const int COL = 10;
         private const int ROW = 10;
 
+        public Hra hra = new Hra();
+
         private List<Rectangle> rectangles;
 
         public GameWindow()
         {
             InitializeComponent();
             rectangles = new List<Rectangle>();
+            for (int x = 0; x < 10; x++)
+            {
+                for(int y = 0; y < 10; y++)
+                {
+                    rectangles.Add(new Rectangle().);
+                }
+            }
         }
         
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -43,6 +52,11 @@ namespace NavalBattle
         private void canvas_player_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             Point mouse = e.GetPosition(canvas_player);
+            int polickoX = (int)Math.Floor(mouse.X / 3);
+            int polickoY = (int)Math.Floor(mouse.Y / 3);
+
+            int indexPolicko = polickoX + (polickoY * 10);
+
         }
     }
 }
